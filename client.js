d6dcb83f8ae1d58900088d7fdbb882145204be2c
Graphmaxer <m.lambotin@gmail.com>
2016-01-23 23:16:52 +0100

    Indentation

--- client.js
+++ client.js
@@ -4,8 +4,8 @@ var pseudo = prompt('Votre pseudo ?') || 'Utilisateur';
 // On se connecte au serveur
 var socket = io.connect("http://partymanagerserver-graphmaxer.rhcloud.com:8000");
   
-  // On creer l'evenement recupererMessages pour recuperer direcement les messages sur serveur
-  socket.on('recupererMessages', function (messages) {
+// On creer l'evenement recupererMessages pour recuperer direcement les messages sur serveur  	
+socket.on('recupererMessages', function (messages) {
 	// messages est le tableau contenant tous les messages qui ont ete ecris sur le serveur
 	var html = '';
 	for (var i = 0; i < messages.length; i++)
@@ -13,24 +13,25 @@ var socket = io.connect("http://partymanagerserver-graphmaxer.rhcloud.com:8000")
 	document.getElementById('tchat').innerHTML = html;
 });
   
-  // Si quelqu'un a poste un message, le serveur nous envoie son message avec l'evenement recupererNouveauMessage
-  socket.on('recupererNouveauMessage', function (message) {
-  	document.getElementById('tchat').innerHTML += '<div class="line"><b>'+message.pseudo+'</b> : '+message.message+'</div>';
-  });
-  // Quand on veut envoyer un message (quand il a valider le formulaire)
-  function envoiMessage(mess) {
+// Si quelqu'un a poste un message, le serveur nous envoie son message avec l'evenement recupererNouveauMessage
+socket.on('recupererNouveauMessage', function (message) {
+	document.getElementById('tchat').innerHTML += '<div class="line"><b>'+message.pseudo+'</b> : '+message.message+'</div>';
+});
+
+// Quand on veut envoyer un message (quand il a valider le formulaire)
+function envoiMessage(mess) {
 	// On recupere le message
 	var message = document.getElementById('message').value;
-	
+
 	// On appelle l'evenement se trouvant sur le serveur pour qu'il enregistre le message et qu'il l'envoie a tous les autres clients connectes (sauf nous)
 	socket.emit('nouveauMessage', { 'pseudo' : pseudo, 'message' : message });
-	
+
 	// On affiche directement notre message dans notre page
 	document.getElementById('tchat').innerHTML += '<div class="line"><b>'+pseudo+'</b> : '+message+'</div>';
-	
+
 	// On vide le formulaire
 	document.getElementById('message').value = '';
-	
+
 	// On retourne false pour pas que le formulaire n'actualise pas la page
 	return false;
 }
--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Tchat avec Socket.IO</title>
-    <script type="text/javascript" src="https://cdn.socket.io/socket.io-1.4.3.js"></script>
-    <script type="text/javascript" src="client.js"></script>
-    <link rel="stylesheet" type="text/css" href="style/main.css">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Tchat avec Socket.IO</title>
+	<script type="text/javascript" src="https://cdn.socket.io/socket.io-1.4.3.js"></script>
+	<script type="text/javascript" src="client.js"></script>
+	<link rel="stylesheet" type="text/css" href="style/main.css">
 </head>
 <body>
-    <h1>Tchat avec Socket.IO</h1>
-    <div id="tchat"></div>
-    <form onsubmit="return (envoiMessage());">
-        <b>Message : </b>
-        <input type="text" name="message" id="message" style="width:250px;" />
-        <input type="submit" value="Envoyer" />
-    </form>
+	<h1>Tchat avec Socket.IO</h1>
+	<div id="tchat"></div>
+	<form onsubmit="return (envoiMessage());">
+		<b>Message : </b>
+		<input type="text" name="message" id="message" style="width:250px;" />
+		<input type="submit" value="Envoyer" />
+	</form>
 
 </body>
 </html>
