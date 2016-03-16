a63f9362349b7debe44695aa03ffdab76ca53c1a
Graphmaxer <m.lambotin@gmail.com>
2016-03-16 11:28:46 +0000

    Lounge hosting started (zerto91)

--- require/loungeHosting.php
+++ require/loungeHosting.php
@@ -1,8 +1,24 @@
 <div id="loungeHosting" class="loungeHostingHided">
-	<h1>LOUNGE HOSTING</h1>
-
-	<div class="chatBox">
-		<h2>Tchat</h2>
+	<div id="loungeHostingMainBox">
+		<div id="loungeHostingSpeakerLeft">
+			<h2>SpeakerLeft</h2>
+		</div>
+		<div id="loungeHostingTrapezeLeft">
+			<h2>TrapezeLeft</h2>
+		</div>
+		<div id="loungeHostingScreen">
+			<h2>Screen</h2>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/unK4kEUmaz0" frameborder="0" allowfullscreen></iframe>
+		</div>
+		<div id="loungeHostingTrapezeRight">
+			<h2>TrapezeRight</h2>
+		</div>
+		<div id="loungeHostingSpeakerRight">
+			<h2>SpeakerRight</h2>
+		</div>
+	</div>
+	<!--<div class="chatBox">
+		<h2>chat</h2>
 		<div class="chat">
 			
 		</div>
@@ -11,5 +27,5 @@
 			<input type="text" class="chatInputMessage" placeholder="Message" autocomplete="off"/><br/>
 			<button class="chatSendButton" type="submit">Envoyer</button>
 		</form>
-	</div>
+	</div>-->
 </div>
