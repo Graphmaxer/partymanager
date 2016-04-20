01d740882a3e31f8fb7f03b45fe0ae240f63f210
Graphmaxer <m.lambotin@gmail.com>
2016-04-20 16:11:52 +0200

    Lounge voting design started

--- require/loungeVoting.php
+++ require/loungeVoting.php
@@ -1,26 +1,32 @@
 <div id="loungeVoting" class="loungeVotingHided">
 	<h1 id="loungeVotingActualLoungeName"></h1>
-	<div id="loungeVotingConnectedAs">Connect&eacute;(e) en tant que : <span id="loungeVotingUserName"></span></div>
-	<div id="loungeVotingLeftBox">
-		<div id="">
-			<input type="text" id="searchInput"/>
-			<div id="searchResult">
+	<div id="loungeVotingMainBox">
+		<div id="loungeVotingConnectedAs">Connect&eacute;(e) en tant que : <span id="loungeVotingUserName"></span></div>
+		<div id="loungeVotingLeftBox">
+			<div id="">
+				<input type="text" id="searchInput"/>
+				<div id="searchResult">
+				</div>
 			</div>
 		</div>
-	</div>
-	
-	<div id="loungeVotingRightBox">
-		<div id="loungeVotingChat">
-		</div>
-		<form onsubmit="return(false)">	
-			<input type="text" id="loungeVotingChatInputMessage" placeholder="Message" autocomplete="off"/><br/>
-			<button id="loungeVotingChatSendButton" type="submit">Envoyer</button>
-		</form>
+		
+		<div id="loungeVotingRightBox">
+			<div id="loungeVotingChatBox">
+				<div id="loungeVotingChat">
+				</div>
+				<form onsubmit="return(false)">	
+					<input type="text" id="loungeVotingChatInputMessage" placeholder="Message" autocomplete="off"/><br/>
+					<button id="loungeVotingChatSendButton" type="submit">Envoyer</button>
+				</form>
+			</div>
 
-		<ul class="userList">
-		</ul>
+			<div id="loungeVotingUserBox">
+				<ul class="userList">
+				</ul>
+			</div>
+		</div>
+		
+		<div id="loungeVotingBack">Quitter le salon</div>
 	</div>
-	
-	<div id="loungeVotingBack">Quitter le salon</div>
 
 </div>
--- script/search.php
+++ script/search.php
@@ -3,18 +3,28 @@
 	$apiKey = "AIzaSyCWgYLX8xehW16dSrgFnT99Z395EldfOLg";
 	$pageToken = "";
 
-	$search = $_POST["searchInput"];
+	$search = htmlspecialchars(urlencode($_POST["searchInput"]));
 
-	$jsonVideos = file_get_contents("https://www.googleapis.com/youtube/v3/search?key=".$apiKey."&part=snippet&type=video&videoCategoryId=10&q=".$search."&maxResults=10&pageToken=".$pageToken);
+	$jsonVideos = file_get_contents("https://www.googleapis.com/youtube/v3/search?key=".$apiKey."&part=snippet&type=video&q=".$search."&maxResults=10&pageToken=".$pageToken);
 
 	$decodedJsonVideos = json_decode($jsonVideos);
 
 	$resultsPerPage = $decodedJsonVideos -> {"pageInfo"} -> {"resultsPerPage"};
 
+	$totalResults = $decodedJsonVideos -> {"pageInfo"} -> {"totalResults"};
+
+	if ($totalResults == 0) {
+		$resultsPerPage = 0;
+
+		echo "<h1>Aucun résultat</h1>";
+	}
+
 	for ($i = 0; $i < $resultsPerPage; $i++) {
 
 		$thumbnailLink = $decodedJsonVideos -> {'items'}[$i] -> {'snippet'} -> {'thumbnails'} -> {'medium'} -> {'url'};
 
-		echo '<img class="video-thumbnail" src="'.$thumbnailLink.'"/>';
+
+		echo '<img class="videoThumbnail" src="'.$thumbnailLink.'"/>';
+
 	}
 ?>
